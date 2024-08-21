dc156cc11df3415f4ffe355b1ae2dba6e0ebd242
mateusz_pikus <mateuszq28@gmail.com>
2024-08-21 03:37:37 +0200

    c

--- querries.sql
+++ querries.sql
@@ -17,7 +17,7 @@ JOIN
 WHERE 
     p3.sex = 'k'
 GROUP BY 
-    p1.person_id
+    p1.person_id, p1.first_name, p1.last_name
 ORDER BY 
     female_grandchildren_count DESC
 LIMIT 1;
@@ -25,6 +25,7 @@ LIMIT 1;
 
 -- Views
 
+DROP VIEW persons_female_grandchildren_count;
 CREATE VIEW persons_female_grandchildren_count AS
     SELECT
         p1.person_id,
@@ -40,7 +41,7 @@ CREATE VIEW persons_female_grandchildren_count AS
     WHERE 
         p3.sex = 'k'
     GROUP BY 
-        p1.person_id
+        p1.person_id, p1.first_name, p1.last_name
 ;
 SELECT * FROM persons_female_grandchildren_count;
 
@@ -318,6 +319,277 @@ CREATE FUNCTION GetPrice (Vendor CHAR(20), Pid INT)
 
     RETURN price; 
 END 
+
+
+
+------------------------v1
+select * from
+{
+dla każdej osoby:
+    znajdź wszystkie osoby, które w polu mother_id lub father_ir mają twoje id
+    dla każdej z tych znalezioncyb osób:
+        dodaj do głównej listy żonę, męża, matkę, ojca if not in list
+    
+    dodaj do tej listy swoją żonę lub męża  if not in list
+
+
+    zarobki = lista.map(p => SuMsalary(p.id))
+
+    sum(zarobki)
+}
+ORDER by sumSalaries ASC
+Limit 1
+
+------------------------v2
+-- select * from
+-- {
+-- dla każdej osoby:
+
+
+--     znajdź wszystkie osoby, które w polu mother_id lub father_ir mają twoje id
+--     dla każdej z tych znalezioncyb osób:
+--         dodaj do głównej listy żonę, męża, matkę, ojca if not in list
+    
+--     dodaj do tej listy swoją żonę lub męża  if not in list
+
+
+    -- zarobki = lista.map(p => SuMsalary(p.id))
+
+    -- sum(zarobki)
+}
+-- ORDER by sumSalaries ASC
+-- Limit 1
+
+
+-- ============================================
+
+
+CREATE VIEW person_with_pointers AS
+SELECT
+p.person_id,
+p.first_name,
+p.last_name
+FROM people p
+LEFT JOIN people m
+ON p.mother_id = m.person_id
+LEFT JOIN people f
+ON p.father_id = f.person_id
+LEFT JOIN people w
+ON p.wife_id = w.person_id
+LEFT JOIN people h
+ON p.husband_id = h.person_id;
+SELECT * FROM person_with_pointers;
+
+
+CREATE VIEW person_with_merriage_pointers AS
+SELECT
+p.person_id,
+p.first_name,
+p.last_name
+FROM people p
+LEFT JOIN people w
+ON p.wife_id = w.person_id
+LEFT JOIN people h
+ON p.husband_id = h.person_id;
+SELECT * FROM person_with_merriage_pointers;
+
+
+DROP VIEW person_with_parent_pointers;
+CREATE VIEW person_with_parent_pointers AS
+SELECT
+p.person_id,
+p.first_name,
+p.last_name
+FROM people p
+LEFT JOIN people m
+ON p.mother_id = m.person_id
+LEFT JOIN people f
+ON p.father_id = f.person_id;
+SELECT * FROM person_with_parent_pointers;
+
+
+-- ===================================================
+
+
+CREATE FUNCTION FIND_PERSON_FAMILY_MEMBERS_LIST (pid int)
+RETURNS TABLE (
+    person_id int,
+    first_name varchar(255),
+    last_name varchar(255)
+)
+LANGUAGE SQL
+READS SQL DATA
+BEGIN
+    RETURN
+    (
+
+        -- self
+
+        SELECT
+        p.person_id,
+        p.first_name,
+        p.last_name
+        FROM people p
+        WHERE p.person_id = pid
+
+        -- wife/husband + children
+        UNION
+
+        SELECT
+        pwp.person_id,
+        pwp.first_name,
+        pwp.last_name
+        FROM person_with_pointers pwp
+        WHERE pwp.m.person_id = pid
+        OR pwp.f.person_id = pid
+        OR pwp.w.person_id = pid
+        OR pwp.h.person_id = pid
+
+        -- UNION
+
+        --- here parents of our childrens
+        --- here merriage of our childrens
+        --- here parents merriage of our childrens
+
+    );
+END;
+
+
+CREATE VIEW family_member_list AS
+SELECT
+FIND_PERSON_FAMILY_MEMBERS_LIST *
+FROM people;
+
+
+CREATE VIEW family_member AS
+fml.person_id,
+fml.first_name,
+fml.last_name,
+fml.SUM_MEMBER_SALARY(person_id) AS member_income
+FROM family_member_list fml;
+
+
+CREATE VIEW person_family_income AS
+SELECT fm.person_id,
+       fm.first_name,
+       fm.last_name,
+       SUM(fm.member_income) AS family_income
+FROM family_member fm
+GROUP BY fm.person_id, fm.idx_first_name, fm.last_name;
+
+
+CREATE VIEW poorest_family AS
+SELECT * FROM person_family_income pfi
+ORDER BY pfi.family_income ASC
+Limit 1;
+
+
+
+==============================================
+
+
+
+
+
+
+
+
+
+
+CREATE FUNCTION person_family_income (Vendor CHAR(20), Pid INT)         
+    RETURNS  DECIMAL(10,3) 
+    LANGUAGE SQL  
+    MODIFIES SQL
+    BEGIN 
+    DECLARE price DECIMAL(10,3); 
+
+    IF Vendor = 'Vendor 1' 
+        THEN SET price = (SELECT ProdPrice FROM V1Table WHERE Id = Pid); 
+    ELSE IF Vendor = 'Vendor 2' 
+        THEN SET price = (SELECT Price 
+                        FROM V2Table 
+                        WHERE Pid = GetPrice.Pid); 
+    END IF; 
+
+    RETURN price; 
+END 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+WITH family_salaries AS (
+    SELECT
+        p.person_id,
+        p.first_name,
+        p.last_name,
+        SUM(e.salary) AS salary,
+        SUM(e1.salary) AS mother_salary,
+        SUM(e2.salary) AS father_salary,
+        SUM(e3.salary) AS wife_salary,
+        SUM(e4.salary) AS husband_salary
+    FROM
+        people p
+    LEFT JOIN
+        employment e ON p.person_id = e.worker_id
+    LEFT JOIN
+        people m ON p.mother_id = m.person_id
+    LEFT JOIN
+        employment e1 ON m.person_id = e1.worker_id
+    LEFT JOIN
+        people f ON p.father_id = f.person_id
+    LEFT JOIN
+        employment e2 ON f.person_id = e2.worker_id
+    LEFT JOIN
+        people w ON p.wife_id = w.person_id
+    LEFT JOIN
+        employment e3 ON w.person_id = e3.worker_id
+    LEFT JOIN
+        people h ON p.husband_id = h.person_id
+    LEFT JOIN
+        employment e4 ON h.person_id = e4.worker_id
+)
+SELECT
+    person_id,
+    first_name,
+    last_name,
+    (salary + mother_salary + father_salary + wife_salary + husband_salary) AS total_family_salary
+FROM
+    family_salaries
+ORDER BY
+    total_family_salary
+LIMIT 1;
+
+
+
+
+
+
+
 
 
 
